46f4a33b13d84d5f50cc5ea1b94237f18d918c35
Tanvi Kale <ktanvarada@gmail.com>
2024-08-09 14:42:46 +0530

    initial file structure commit

--- src/main/java/com/Neueda/PortfolioManagementBackend/model/OrderBookBond.java
+++ src/main/java/com/Neueda/PortfolioManagementBackend/model/OrderBookBond.java
@@ -1,27 +1,27 @@
-package com.Neueda.PortfolioManagementBackend.model;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-
-import java.util.Date;
-
-@Entity
-public class OrderBookBond {
-    @OneToOne
-    @JoinColumn(name = "ticker_symbol", referencedColumnName = "ticker_symbol")
-    private Bond bond;
-
-    @Column(name = "asset_name")
-    private String asset_name;
-
-    @Column(name = "transaction_date")
-    private Date transaction_date;
-
-    @Column(name = "bond_price")
-    private Integer bond_price;
-
-    @Column(name = "maturity_date")
-    private Date maturityDate;
-}
+//package com.Neueda.PortfolioManagementBackend.model;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.OneToOne;
+//
+//import java.util.Date;
+//
+//@Entity
+//public class OrderBookBond {
+//    @OneToOne
+//    @JoinColumn(name = "ticker_symbol", referencedColumnName = "ticker_symbol")
+//    private Bond bond;
+//
+//    @Column(name = "asset_name")
+//    private String asset_name;
+//
+//    @Column(name = "transaction_date")
+//    private Date transaction_date;
+//
+//    @Column(name = "bond_price")
+//    private Integer bond_price;
+//
+//    @Column(name = "maturity_date")
+//    private Date maturityDate;
+//}
--- src/main/java/com/Neueda/PortfolioManagementBackend/model/OrderBookStock.java
+++ src/main/java/com/Neueda/PortfolioManagementBackend/model/OrderBookStock.java
@@ -1,25 +1,25 @@
-package com.Neueda.PortfolioManagementBackend.model;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-
-import java.util.Date;
-
-public class OrderBookStock {
-    @OneToOne
-    @JoinColumn(name = "ticker_symbol", referencedColumnName = "ticker_symbol")
-    private Stock stock;
-
-    @Column(name = "asset_name")
-    private String asset_name;
-
-    @Column(name = "volume")
-    private Integer volume;
-
-    @Column(name = "transaction_date")
-    private Date transaction_date;
-
-    @Column(name = "bought_price")
-    private Integer bought_price;
-}
+//package com.Neueda.PortfolioManagementBackend.model;
+//
+//import jakarta.persistence.Column;
+//import jakarta.persistence.JoinColumn;
+//import jakarta.persistence.OneToOne;
+//
+//import java.util.Date;
+//
+//public class OrderBookStock {
+//    @OneToOne
+//    @JoinColumn(name = "ticker_symbol", referencedColumnName = "ticker_symbol")
+//    private Stock stock;
+//
+//    @Column(name = "asset_name")
+//    private String asset_name;
+//
+//    @Column(name = "volume")
+//    private Integer volume;
+//
+//    @Column(name = "transaction_date")
+//    private Date transaction_date;
+//
+//    @Column(name = "bought_price")
+//    private Integer bought_price;
+//}
